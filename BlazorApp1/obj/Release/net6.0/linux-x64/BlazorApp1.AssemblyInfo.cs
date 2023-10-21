8e1ce6113fbb5738a0adbdea16cc7df19b9fa272
o.m777@email.cz <malik@spsejecna.cz>
2023-10-21 20:18:43 +0200

    linux release 2

--- BlazorApp1/obj/Release/net6.0/linux-x64/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Release/net6.0/linux-x64/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10c3c3078415a214c5dd0d8cc62927b2a0cd312a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee781037bad10e8e700ff42a0f5a06d6398c122e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
