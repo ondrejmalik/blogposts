068736484ba9ac2c2b8caa2aadd263bfe3682e45
o.m777@email.cz <malik@spsejecna.cz>
2023-10-25 23:31:06 +0200

    remote sql connection

--- BlazorApp1/Shared/Credentials.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BlazorApp1.Shared;
-
-public static class Credentials
-{
-    public static string username;
-    public static string password;
-    public static bool loggedIn;
-    public static bool firstTime = true;
-}
