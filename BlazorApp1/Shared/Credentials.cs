4441de8715f091ce4d5cea1923292fed1275672c
o.m777@email.cz <malik@spsejecna.cz>
2023-10-25 22:00:54 +0200

    before cleanup

---
+++ BlazorApp1/Shared/Credentials.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp1.Shared;
+
+public static class Credentials
+{
+    public static string username;
+    public static string password;
+    public static bool loggedIn;
+    public static bool firstTime = true;
+}
