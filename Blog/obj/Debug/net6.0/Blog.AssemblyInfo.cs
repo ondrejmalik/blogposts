6b185d445dc3fbf7bd5753038ca204b3e0c5be01
o.m777@email.cz <malik@spsejecna.cz>
2023-10-26 00:28:24 +0200

    portable build

--- Blog/obj/Debug/net6.0/Blog.AssemblyInfo.cs
+++ Blog/obj/Debug/net6.0/Blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+295776ed66dcffa4aeed2dccb98a273adf82083b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
